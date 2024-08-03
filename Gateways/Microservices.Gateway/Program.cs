c779fc7559748e4ebabe0b652b4ab427627ef112
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2024-08-03 23:44:42 +0300

    ocelot gateway implemented

---
+++ Gateways/Microservices.Gateway/Program.cs
@@ -0,0 +1,29 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddJsonFile($"configuration.{builder.Environment.EnvironmentName}.json")
+    .AddEnvironmentVariables();
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+
+
+
+
+app.MapGet("/", () => "Hello World!");
+
+
+await app.UseOcelot();
+
+app.Run();
+
+
