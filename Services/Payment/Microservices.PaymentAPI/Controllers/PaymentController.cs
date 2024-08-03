1586e2085e135e6d0e4e7571a5e13c183c85de43
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2024-08-03 22:59:53 +0300

    fake payment service developed

---
+++ Services/Payment/Microservices.PaymentAPI/Controllers/PaymentController.cs
@@ -0,0 +1,16 @@
+﻿using Microservices.Shared.Base;
+using Microservices.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.PaymentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+            => ControllerResponse(ServiceResponse<NoContent>.Success(204));
+    }
+}
